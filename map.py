119ad350c451b458c5aa50de3f2fde43295cbfa7
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-05-02 17:29:00 +0530

    population

--- map.py
+++ map.py
@@ -15,13 +15,22 @@ def color_producer(risk):
 	else:
 		return 'green'
 
-map = folium.Map([38.58, -99.09], zoom_start=6, tiles="mapbox Bright")
+map = folium.Map([38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for i,j,k in zip(lat, long, risk):
-	fg.add_child(folium.Marker(location=[i,j], popup="Risk Factor: " + str(k), icon=folium.Icon(color=color_producer(k))))
+	fgv.add_child(folium.Marker(location=[i,j], popup="Risk Factor: " + str(k), icon=folium.Icon(color=color_producer(k))))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save('map.html')
